0cbe2b5980381d16f4430d2eac1a569a7c6b3bc7
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-11-21 18:26:33 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,59 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handlePrevious = () => {
+    let index = currentIndex;
+    if (index === 0) {
+      return;
+    }
+    setCurrentIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    let index = currentIndex;
+    if (index === 4) {
+      return;
+    }
+    setCurrentIndex(index + 1);
+  };
+
+  const handleRestart = () => {
+    setCurrentIndex(0);
+  };
+
   return (
-    <></>
-  )
-}
-
+    <>
+      <div>
+        <h1 data-testid="title">{slides[currentIndex].title}</h1>
+        <p data-testid="text">{slides[currentIndex].text}</p>
+      </div>
+      <div>
+        <button
+          disabled={currentIndex === 0}
+          onClick={handlePrevious}
+          data-testid="button-prev"
+        >
+          Previous Slide
+        </button>
+        <button
+          disabled={currentIndex === 4}
+          onClick={handleNext}
+          data-testid="button-next"
+        >
+          Next Slide
+        </button>
+        <button
+          disabled={currentIndex === 0}
+          onClick={handleRestart}
+          data-testid="button-restart"
+        >
+          Restart Slide
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default App;
